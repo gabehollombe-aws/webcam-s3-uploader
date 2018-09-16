634853c42a462bd1e847fbf7cea0fa2c1356055e
Gabe Hollombe <gabe@avantbard.com>
2018-09-16 17:49:36 +0700

    add labeling prefix, capture interval, and nicer toggling

--- src/App.js
+++ src/App.js
@@ -6,43 +6,53 @@ import {v4 as uuid} from 'uuid';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-const uploadImageToS3 = async (imgSrc) => {
+const uploadImageToS3 = async (imgSrc, pathPrefix) => {
   const credentials = await Auth.currentCredentials();
   const s3 = new AWS.S3({
       apiVersion: '2013-04-01',
       credentials: Auth.essentialCredentials(credentials)
     });
 
-
   const base64Data = new Buffer(imgSrc.replace(/^data:image\/\w+;base64,/, ""), 'base64')
-
   const type = imgSrc.split(';')[0].split('/')[1]
 
   const params = {
     Bucket: aws_exports.aws_user_files_s3_bucket,
-    Key: `public/${uuid()}.${type}`,
+    Key: `${pathPrefix}/${uuid()}.${type}`,
     Body: base64Data,
-    // ACL: 'public-read',
-    ContentEncoding: 'base64', // required
+    ContentEncoding: 'base64',
     ContentType: `image/${type}`
   }
 
-  console.log('Uploading...')
-  const result = await s3.upload(params).promise();
-  console.dir(result);
+  return s3.upload(params).promise();
 }
 
 
 class WebcamCapture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      captureInterval: null,
+      label: ''
+    }
+  }
   setRef = webcam => {
     this.webcam = webcam;
   };
  
   capture = () => {
     const imageSrc = this.webcam.getScreenshot();
-    uploadImageToS3(imageSrc);
+    uploadImageToS3(imageSrc, `public/${this.state.label}`);
   };
- 
+
+  toggleCapture = () => {
+    this.state.captureInterval
+      ? this.setState({ captureInterval: window.clearInterval(this.state.captureInterval) })
+      : this.setState({ captureInterval: window.setInterval(this.capture, this.props.captureInterval) })
+  }
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
+
   render() {
     const videoConstraints = {
       width: 1280,
@@ -52,15 +62,25 @@ class WebcamCapture extends React.Component {
  
     return (
       <div>
-        <Webcam
-          audio={false}
-          height={350}
-          ref={this.setRef}
-          screenshotFormat="image/jpeg"
-          width={350}
-          videoConstraints={videoConstraints}
-        />
-        <button onClick={this.capture}>Capture photo</button>
+        <div>
+          <Webcam
+            audio={false}
+            height={350}
+            ref={this.setRef}
+            screenshotFormat="image/jpeg"
+            screenshotWidth={224} // no sense capturing images in a resolution higher than what resnet wants
+            width={350}
+            videoConstraints={videoConstraints}
+          />
+        </div>
+        <input placeholder='Enter a label name' type='text' onChange={this.handleChange} name='label' disabled={this.state.captureInterval} />
+        <button onClick={this.toggleCapture} disabled={this.state.label === ''}>
+          {
+            this.state.captureInterval
+              ? 'Stop Capturing'
+              : 'Begin Capturing'
+            }
+        </button>
       </div>
     );
   }
@@ -70,7 +90,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <WebcamCapture />
+        <WebcamCapture captureInterval={300}/>
       </div>
     );
   }
