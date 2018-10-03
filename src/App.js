f27d8d51942eb0bea3083fcb55982af1b515b1f4
Gabe Hollombe <gabe@avantbard.com>
2018-10-03 15:58:43 +0800

    allow deleting by double-clicking

--- src/App.js
+++ src/App.js
@@ -34,12 +34,19 @@ const uploadImageToS3 = async (imgSrc, pathPrefix) => {
   return params.Key;
 }
 
-const getImageKeys = async (datasetName) => {
-  console.log('Getting image keys for ' + datasetName)
+const removeImageFromS3 = async (key) => {
+  const s3 = await S3();
+  return s3.deleteObject({
+    Bucket: aws_exports.aws_user_files_s3_bucket,
+    Key: key
+  }).promise()
+}
+
+const getImageKeys = async (datasetName, label) => {
   const s3 = await S3();
   const result = await s3.listObjectsV2({
     Bucket: aws_exports.aws_user_files_s3_bucket,
-    Prefix: `public/${datasetName}/`,
+    Prefix: `public/${datasetName}/${label}/`,
   }).promise();
   return result.Contents.map(o => o.Key);
 }
@@ -74,6 +81,7 @@ class WebcamCapture extends React.Component {
 
     return (
       <div>
+        <h3>Press Enter to add webcam capture</h3>
         <div>
           <Webcam
             audio={false}
@@ -85,9 +93,6 @@ class WebcamCapture extends React.Component {
             videoConstraints={videoConstraints}
           />
         </div>
-        <Form.Button onClick={this.handleCapture} disabled={this.props.disabled}>
-          Capture
-        </Form.Button>
       </div>
     );
   }
@@ -99,8 +104,12 @@ class ImageWrapper extends Component {
   }
 
   render() {
-    if (this.props.src) return <Image src={this.props.src} />;
-    return <S3Image style={{display: 'inline-block'}} imgKey={this.props.imgKey.replace(/^public\//, '')} />
+    if (this.props.src) return <Image src={this.props.src} onDoubleClick={this.props.onDoubleClick} />;
+    return (
+      <div style={{display: 'inline-block'}} onDoubleClick={this.props.onDoubleClick} >
+        <S3Image style={{display: 'inline-block', margin: '0 .25rem'}} imgKey={this.props.imgKey.replace(/^public\//, '')}/>
+      </div>
+    )
   }
 }
 
@@ -108,8 +117,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      label: '',
       datasetName: '',
+      label: '',
+      fetching: false,
       totalUploads: 0,
       uploadsCompleted: 0,
       existingImageKeys: [],
@@ -117,16 +127,43 @@ class App extends Component {
     };
   }
 
-  fetchImageKeys = async (datasetName) => {
+  fetchImageKeys = async (datasetName, label) => {
     if (datasetName === '') return;
 
-    const existingImageKeys = await getImageKeys(datasetName);
-    this.setState({ existingImageKeys })
+    this.setState({
+      fetching: true
+    });
+
+    const existingImageKeys = await getImageKeys(datasetName, label);
+    this.setState({ 
+      existingImageKeys, 
+      fetching: false
+    })
   }
 
   handleChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
-  handleDatasetBlur = (e) => this.fetchImageKeys(e.target.value);
+  handleLabelChange = (e) => {
+    this.handleChange(e);
+    this.fetchImageKeys(this.state.datasetName, e.target.value);
+    this.setState({
+      uploadedImages: {}
+    });
+  }
+
+  handleImageDoubleClick = (key) => {
+    removeImageFromS3(key);
+
+    const updatedUploadedImages = Object.assign(this.state.uploadedImages, {});
+    delete updatedUploadedImages[key];
+
+    const updatedExistingImageKeys = this.state.existingImageKeys.filter(k => k !== key);
+
+    this.setState({
+      uploadedImages: updatedUploadedImages,
+      existingImageKeys: updatedExistingImageKeys,
+    });
+  }
 
   upload = async (imageSrc) => {
     if (this.state.label === '') return;
@@ -144,7 +181,6 @@ class App extends Component {
 
   render() {
     const UploadStatus = () => {
-      if (this.state.totalUploads === 0) return <div>Waiting for capture...</div>
       return (
         <div>
           Uploaded {this.state.uploadsCompleted} of {this.state.totalUploads}
@@ -156,21 +192,31 @@ class App extends Component {
       <div>
         <Form>
           <Form.Group widths='equal'>
-            <Form.Input label='Dataset Name' placeholder='Dataset Name' name='datasetName' onChange={this.handleChange} onBlur={this.handleDatasetBlur} />
-            <Form.Input label='Label' placeholder='Label' name='label' onChange={this.handleChange} />
+            <Form.Input label='Dataset Name' placeholder='Dataset Name' name='datasetName' onChange={this.handleChange} />
+            <Form.Input label='Label' placeholder='Label' name='label' onChange={this.handleLabelChange} />
           </Form.Group>
           
-          <Form.Group>
-            <WebcamCapture onCapture={this.upload} disabled={this.state.label === ''}/>
-          </Form.Group>
-
-          <UploadStatus />
-
-          <Image.Group size='tiny'>
-            { this.state.existingImageKeys.map(key => <ImageWrapper key={key} imgKey={key} />) }
-            { Object.keys(this.state.uploadedImages).map(key => <ImageWrapper key={key} src={this.state.uploadedImages[key]} />) }
-          </Image.Group>
+          <WebcamCapture onCapture={this.upload} disabled={this.state.label === ''}/>
         </Form>
+
+        {/* <UploadStatus /> */}
+
+        { (this.state.existingImageKeys.length > 0 || Object.keys(this.state.uploadedImages).length > 0)
+          ? <h3>Double-click an image to remove it</h3>
+          : null
+        }
+
+        <Image.Group size='tiny'>
+          { 
+            this.state.fetching
+            ? <h3>Loading existing images...</h3>
+            : this.state.existingImageKeys.map(key => <ImageWrapper key={key} imgKey={key} onDoubleClick={this.handleImageDoubleClick.bind(this, key)} />)
+          }
+        </Image.Group>
+
+        <Image.Group size='tiny'>
+          { Object.keys(this.state.uploadedImages).map(key => <ImageWrapper key={key} src={this.state.uploadedImages[key]} onDoubleClick={this.handleImageDoubleClick.bind(this, key)} />) }
+        </Image.Group>
       </div>
     );
   }
